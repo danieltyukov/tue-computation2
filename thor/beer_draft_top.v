a42c033cdf2838853d728efc4bbd82d37229d3cb
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2024-04-11 21:42:56 +0200

    update final thor stuff

--- thor/beer_draft_top.v
+++ thor/beer_draft_top.v
@@ -1,14 +1,132 @@
 `timescale 1ns / 1ps
 
 module beer_draft_top(
-    clk,
-    reset,
-    next,draft,
-    beer_level,
-    beer, 
-    state_display);
-        
-      // WRITE LOGIC HERE
-        
+  input wire [0:0] clk,
+  input wire [0:0] reset,
+  input wire [0:0] next,
+  input wire [0:0] draft,
+  input wire [1:0] beer_level,
 
+  output reg [0:0] beer, 
+  output reg [1:0] state_display
+  );
+
+  // states
+  localparam
+  s_off = 0,
+  s_foam = 1,
+  s_beer = 2;
+
+  localparam
+  LOW = 1'b0,
+  HIGH = 1'b1;
+
+  // memory
+  reg [1:0] state;
+  reg [1:0] state_next;
+  reg [0:0] next_prev;
+
+  always @(posedge clk) begin
+    // reset on 1 means reset on 0
+    if (reset == HIGH) begin
+        state <= s_off;
+        next_prev <= LOW;
+    end
+    else begin
+      if (next == HIGH && next_prev == LOW) begin
+        state <= state_next;
+      end
+      next_prev <= next;
+    end
+  end
+
+  always @(*) begin
+    state_next = state;
+    case (state)
+      s_off: begin
+        if (draft == HIGH && beer_level == 0) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 0) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 1) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 2) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 3) begin
+          state_next = s_off;
+        end
+
+        if (draft == HIGH && beer_level == 1) begin
+          state_next = s_foam;
+        end
+        if (draft == HIGH && beer_level == 2) begin
+          state_next = s_foam;
+        end
+        if (draft == HIGH && beer_level == 3) begin
+          state_next = s_foam;
+        end
+      end
+      s_foam: begin
+        if (draft == HIGH && beer_level == 1) begin
+          state_next = s_foam;
+        end
+        if (draft == HIGH && beer_level == 3) begin
+          state_next = s_foam;
+        end
+
+        if (draft == HIGH && beer_level == 2) begin
+          state_next = s_beer;
+        end
+
+        if (draft == HIGH && beer_level == 0) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 0) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 1) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 2) begin
+          state_next = s_off;
+        end
+        if (draft == LOW && beer_level == 3) begin
+          state_next = s_off;
+        end
+      end
+      s_beer: begin
+        if (draft == HIGH && beer_level == 2) begin
+          state_next = s_beer;
+        end
+
+        if (draft == LOW && beer_level == 1) begin
+          state_next = s_foam;
+        end
+        if (draft == LOW && beer_level == 2) begin
+          state_next = s_foam;
+        end
+        if (draft == HIGH && beer_level == 1) begin
+          state_next = s_foam;
+        end
+      end
+    endcase
+    case (state)
+      s_off: begin
+        state_display = s_off;
+        beer = LOW;
+      end
+      s_foam: begin
+        state_display = s_foam;
+        beer = LOW;
+      end
+      s_beer: begin
+        state_display = s_beer;
+        beer = HIGH;
+      end
+    endcase
+  end
 endmodule
--- thor/beer_draft_top_tb.v
+++ thor/beer_draft_top_tb.v
@@ -1,7 +1,23 @@
+// state 0, beer 0
+// state 1, beer 0
+// state 1, beer 0
+// state 2, beer 1
+// state 2, beer 1
+// state 0, beer 0
+// state 1, beer 0
+// state 0, beer 0
+
 `timescale 1ns / 1ps
 module beer_draft_top_tb();
 
-	<<DECLARE SIGNALS TO DRIVE MODULE beer_draft_top>>
+	// <<DECLARE SIGNALS TO DRIVE MODULE beer_draft_top>>
+	reg clk;
+	reg reset;
+	reg next;
+	reg draft;
+	reg [1:0] beer_level;
+	wire beer;
+	wire [1:0] state_display;
    
 	beer_draft_top beer_draft_top_inst(
 		.clk(clk), 
@@ -11,28 +27,79 @@ module beer_draft_top_tb();
 		.beer_level(beer_level), 
 		.beer(beer), 
 		.state_display(state_display));
-   <<DECLARE CLOCK>>
+
+	//    <<DECLARE CLOCK>>
+	always #10 clk=~clk;
    
            
    initial begin
 	  $dumpfile("dut.vcd");
    	  $dumpvars(0, beer_draft_top_inst);
 	  
-	  <<RESET>>
+	  //   <<RESET>>
+	  //   <<STIMULI 1>>
+	  clk = 0;
+	  next = 0;
+	  draft = 0;
+	  beer_level = 0;
+	  reset = 1;
 	  #500
 	  $display("state %x, beer %b", state_display,beer);
-   
-	  
-	  <<STIMULI 1>>
-	  #500
-	  $display("state %x, beer %b", state_display,beer);
+	  reset = 0;
 
-	  <<STIMULI 2>>
+	  //   <<STIMULI 2>>
+	  draft = 1;
+	  beer_level = 1;
+	  next = 0;
+	  #50 next = 1;
 	  #500
 	  $display("state %x, beer %b", state_display,beer);
 	   
-	  ...................  continue for the rest of the stimuli .............................
-	  
+	  //   ...................  continue for the rest of the stimuli .............................
+	  draft = 1;
+	  beer_level = 1;
+	  next = 0;
+	  #50 next = 1;
+	  #500
+	  $display("state %x, beer %b", state_display,beer);
+
+	  draft = 1;
+	  beer_level = 2;
+	  next = 0;
+	  #50 next = 1;
+	  #500
+	  $display("state %x, beer %b", state_display,beer);
+
+	  draft = 1;
+	  beer_level = 2;
+	  next = 0;
+	  #50 next = 1;
+	  #500
+	  $display("state %x, beer %b", state_display,beer);
+
+	  clk = 0;
+	  next = 0;
+	  draft = 0;
+	  beer_level = 0;
+	  reset = 1;
+	  #500
+	  $display("state %x, beer %b", state_display,beer);
+	  reset = 0;
+
+	  draft = 1;
+	  beer_level = 1;
+	  next = 0;
+	  #50 next = 1;
+	  #500
+	  $display("state %x, beer %b", state_display,beer);
+
+	  draft = 1;
+	  beer_level = 0;
+	  next = 0;
+	  #50 next = 1;
+	  #500
+	  $display("state %x, beer %b", state_display,beer);
+
 	  #100
       $finish;
    end
